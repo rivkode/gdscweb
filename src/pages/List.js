041dc92f733fd962e65c9b24b7e501b850c9d913
jonghun <jonghuncu@gmail.com>
2022-10-13 18:21:45 +0900

    make lists

---
+++ src/pages/List.js
@@ -0,0 +1,35 @@
+function List() {
+  return(
+    <table class="table table-striped table-hove">
+        <thead>
+          <tr>
+            <th scope="col">번호</th>
+            <th scope="col">제목</th>
+            <th scope="col">글쓴이</th>
+            <th scope="col">날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td colspan="2">첫번째 글이고 싶어요</td>
+            <td>Otto</td>
+            <td>@22-10-10</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td colspan="2">두번째 글인가요</td>
+            <td>Thornton</td>
+            <td>@22-10-8</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">난 세번째</td>
+            <td>river</td>
+            <td>@22-10-3</td>
+          </tr>
+        </tbody>
+      </table>
+  )
+}
+export default List
